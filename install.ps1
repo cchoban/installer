1158e2f4f87317f2f6e0464adc5be80cddba779b
Muhammed Kaplan <gokdeniz91@gmail.com>
2018-06-04 15:30:40 +0200

    use powershell script instead of python one

---
+++ install.ps1
@@ -0,0 +1,90 @@
+﻿Import-Module BitsTransfer
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Host "Please run script as Administrator." -f red
+    exit
+}
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    Write-Host "Unzipping chob.zip..." -f Cyan
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+    Write-Host "Successfully unzipped." -f Green
+}
+
+
+function downloadFile($url, $outPath){
+    $start_time = Get-Date
+    Start-BitsTransfer -Source $url -Destination $outPath 
+    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+}
+
+function Get-PSVersion {
+    if (test-path variable:psversiontable){
+        $psversiontable.psversion
+    }else {
+        [version]"1.0.0.0"
+    }
+}
+
+function checkArch {
+    if ($is64bit = [Environment]::Is64BitOperatingSystem)
+    {
+        return $is64bit
+    }
+}
+
+if (Get-PSVersion) {
+    $scriptRoot = $PSScriptRoot
+}else {
+    $scriptRoot = split-path -parent $MyInvocation.MyCommand.Definition
+}
+
+$path = $scriptRoot + "\.choban"
+
+Write-Output $path
+if (!(Test-Path $path)){
+    New-Item -ItemType Directory -Force -Path $path
+    Write-Host "Created directory $path " -f green
+}
+
+
+if (checkArch){
+    $pythonUrl = "https://www.python.org/ftp/python/3.6.5/python-3.6.5-amd64.exe"
+}else {
+    $pythonUrl = "https://www.python.org/ftp/python/3.6.5/python-3.6.5.exe"
+}
+
+
+if(!(Test-Path $path"\python3.exe") -and !(Test-Path $path"\chob.zip"))
+{
+    Write-Host "Downloading Python 3 from $pythonUrl..." -f cyan
+    downloadFile -url $pythonUrl -outPath $scriptRoot\.choban\python3.exe
+    downloadFile -url "http://mrmkaplan.com/chob.zip" -outPath $path
+}
+
+
+Write-Host "Installing Python 3.." -f cyan
+#$p = Start-Process $path\python3.exe -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -wait -NoNewWindow -PassThru
+#if ($p.HasExited -and ($p.ExitCode -eq 0)){
+#    Write-Host "Sucessfully installed Python 3 " -f green
+#}else {
+#    Write-Host "Could not install Python 3 " -f red
+#}
+
+
+if(!(Test-Path $path\choban\programData)) {
+    Unzip $path\chob.zip $path\choban\programData
+}
+
+
+
+Write-Host "Setting correct environments.." -f Cyan
+[Environment]::SetEnvironmentVariable("chobanPath","$env:programdata\choban", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("chobanTools","$env:SystemDrive\tools", [EnvironmentVariableTarget]::Machine)
+cmd /c $path\choban\programData\refreshenv.cmd
+
+Copy-Item $path\choban\programData\ -Recurse -Destination  "$env:programdata\choban" -Container
+Write-Host "Sucessfully installed Choban" -f Green
