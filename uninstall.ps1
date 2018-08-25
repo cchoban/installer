c4478d3c17b08ee88d47056220cc8c302f3ebdd9
Muhammed Kaplan <gokdeniz91@gmail.com>
2018-08-25 11:59:29 +0200

    Simple uninstaller for Choban package manager

---
+++ uninstall.ps1
@@ -0,0 +1,14 @@
+$chobanPath = $env:ProgramData + '\choban1'
+
+if (Test-Path($chobanPath)) {
+    Write-Host 'Deleting '$chobanPath -f Cyan
+    Remove-Item $chobanPath -Force -Recurse
+}
+
+Write-Host 'Deleting Envoirement Variables' -f Cyan
+[Environment]::SetEnvironmentVariable("chobanTools", $null, "Machine")
+[Environment]::SetEnvironmentVariable("chobanCli", $null, "Machine")
+[Environment]::SetEnvironmentVariable("chobanPath", $null, "Machine")
+
+
+Write-Host 'Successfully uninstalled Choban from your computer :(' -f Green
